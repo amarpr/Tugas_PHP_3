c9ee9fccee61a9a7190d1f67d19606731033e296
Muhamar <amararrasyid@gmail.com>
2020-05-30 20:43:52 +0700

    Tugas 3 PHP
    Mengerjakan Tugas Niomic

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+$angka_pertama = 360;
+$angka_kedua = 90;
+
+$hasil = $angka_pertama / $angka_kedua;
+
+echo " Jarak Kota A - Z = $angka_pertama <br>";
+echo "Kecepatan Motor = $angka_kedua <br>";
+echo "Waktu Tempuh = ....? <br>";
+echo "Jawaban : $hasil Menit";
+?>
---
+++ index2.php
@@ -0,0 +1,12 @@
+<?php
+$angka_pertama = 150000;
+$angka_kedua = 12.5;
+
+$hasil1 = $angka_pertama * $angka_kedua / 100;
+$hasil2 = $angka_pertama + $hasil1;
+
+echo "Tabungan Awal = Rp. $angka_pertama <br>";
+echo "Bunga Setahun = $angka_kedua % <br>";
+echo "Total Tabungan Setahun = ....? <br>";
+echo "jawaban : Rp. $hasil2"
+?>
